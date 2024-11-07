968d1d7727c6e736a36958af9e5ebd0efa24ca7e
Daniel Li <daniel39@illinois.edu>
2024-11-07 17:41:17 -0600

    Added server env file and tested client/server interactions

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
 import logo from './logo.svg';
 import styles from './App.module.scss';
+import { useEffect } from 'react';
 
 function App() {
+
   return (
     <div className={styles.App}>
       <header className={styles["App-header"]}>
@@ -18,9 +19,24 @@ function App() {
         >
           Learn React
         </a>
+        <div onClick={testServer}>Test Server</div>
       </header>
     </div>
   );
+}
+
+async function testServer() {
+  try {
+    //CORS is annoying.. will need to figure out later
+    const response = await fetch('http://localhost:5050/api', {
+      mode: 'cors',
+      method: 'GET'
+    })
+    const data = await response.json();
+    console.log(response, data)
+  } catch (e: unknown) {
+    console.log(e)
+  }
 }
 
 export default App;
