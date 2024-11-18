4ae09f59922e079be93224e25dfca93e1651c80d
Daniel Li <daniel39@illinois.edu>
2024-11-17 19:33:30 -0600

    Auth wip

--- client/src/components/Profile/Profile.tsx
+++ client/src/components/Profile/Profile.tsx
@@ -1,8 +1,10 @@
-import React, { useState, useEffect } from "react";
-import style from "./profile.module.css"
+import React, { useEffect, useState } from "react";
+import style from "./Profile.module.css"
 import defaultPfp from "./DefaultPFP.jpg"
 import Calendar from "../Calendar/Calendar"
-
+import { auth } from "../../services/auth";
+import { Navigate } from "react-router-dom";
+import { UserCredential } from "firebase/auth";
 export interface User {
     name: string;
     bio?: string;
@@ -11,7 +13,7 @@ export interface User {
     location?: string;
     followers: string[];
     following: string[];
-    followsDiet: DietDetails | null;
+    followsDiet?: DietDetails;
     completedDiets: number;
     dietsCreated: string[];
 }
@@ -23,35 +25,61 @@ export interface DietDetails {
     repeating: boolean;
 }
 
+export const EmptyUser = {
+    name: "",
+    followers: [],
+    following: [],
+    completedDiets: 0,
+    dietsCreated: [],
+};
+
 function Profile() {
-    const [name, setName] = useState('John');
+    //Replace with EmptyUser later
+    const [user, setUser] = useState<User>({
+        name: "John",
+        followers: [],
+        following: [],
+        followsDiet: {
+            diet: "test",
+            dietStarted: new Date(),
+            daysCompleted: [],
+            repeating: false,
+        },
+        completedDiets: 0,
+        dietsCreated: [],
+    });
+
+    useEffect(() => {
+        const userDetails = auth.currentUser;
+        
+    });
 
     return (
         <div className={style.container}>
             {/* <!-- Cover Photo and Profile Picture --> */}
             <div className={style.coverPhoto}>
                 <div className={style.profilePhoto}>
-                    <img src={defaultPfp} alt="Profile Photo"/>
+                    <img src={defaultPfp} alt="Profile"/>
                 </div>
             </div>
 
             {/* <!-- User Info Section --> */}
             <div className={style.userInfo}>
-                <h1>{name}</h1>
-                <p>Profile bio here</p>
+                <h1>{user.name}</h1>
+                <p>{user.bio ?? "No bio specified."}</p>
             </div>
             <div className={style.mainContent}>
                 <div className={style.dietContent}>
                     {/* <!-- Diet Section --> */}
                     <div className={style.currentDietSection}>
-                        <h2>{name}'s Current Diet</h2>
+                        <h2>{user.name}'s Current Diet</h2>
                         <div className={style.currentDiet}>
                             <Calendar user={null} calendarId={''}/>
                         </div>
                     </div>
 
                     <div className={style.createdDietsSection}>
-                        <h2>{name}'s Created Diets</h2>
+                        <h2>{user.name}'s Created Diets</h2>
                         <div className={style.createdDiets}>
                             <p>Insert created diet calendars here</p>
                         </div>
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,40 +1,24 @@
-import React, { StrictMode } from 'react';
+import { StrictMode, createContext, useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { createBrowserRouter, RouterProvider, Route, createRoutesFromElements, Navigate } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
 import Home from './components/Home/Home';
 import Profile from './components/Profile/Profile';
+import { Authorize } from './services/auth';
+import { getAuth, onAuthStateChanged, UserInfo } from 'firebase/auth';
+
 //import reportWebVitals from './reportWebVitals';
 
-// Define the client side pages for the app
-// This defines the following pages and their urls:
-// -> /home, /profile, /editor, /search, /login
-// /home contains the home feed. Currently no nested routes planned
-// /profile contains the user's profile and current diet calendar
-// /editor contains the diet calendar editor
-// /search contains the searching and query page for searching users/calendars
-// /login handles signing in and/or signing up
-const router = createBrowserRouter(createRoutesFromElements([
-  <Route path="/" element={<NavBar/>}>
-    <Route index element={<Navigate to="/home" replace={true}/>}/>
-    <Route path="/home" element={<Home/>}/>
-    <Route path="/profile" element={<Profile/>}/>
-    <Route path="/editor" element={<div>Not implemented.</div>}/>
-    <Route path="/search" element={<div>Not implemented.</div>}/>
-    <Route path="/login" element={<div>Not implemented.</div>}/>
-  </Route>,
-  <Route path="/*" element={<div>404 not found.</div>}/>
-]));
+export const AuthContext = createContext<UserInfo | null>(null);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-
 root.render(
   <StrictMode>
-    <RouterProvider router={router}/>
+    <App/>
   </StrictMode>
 );
 
@@ -43,3 +27,41 @@ root.render(
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 //Web vitals is broken idk
 //reportWebVitals();
+
+function App() {
+  const auth = getAuth();
+  const [user, setUser] = useState<UserInfo | null>(null);
+  onAuthStateChanged(auth, (userInfo) => {
+    if (userInfo) {
+      setUser(userInfo);
+    } else {
+      setUser(null);
+    }
+  })
+
+  // Define the client side pages for the app
+  // This defines the following pages and their urls:
+  // -> /home, /profile, /editor, /search, /login
+  // /home contains the home feed. Currently no nested routes planned
+  // /profile contains the user's profile and current diet calendar
+  // /editor contains the diet calendar editor
+  // /search contains the searching and query page for searching users/calendars
+  // /login handles signing in and/or signing up
+  const router = createBrowserRouter(createRoutesFromElements([
+    <Route path="/" element={<NavBar/>}>
+      <Route index element={<Navigate to="/home" replace={true}/>}/>
+      <Route path="/home" element={<Home/>}/>
+      <Route path="/profile" element={<Authorize component={<Profile/>}/>}/>
+      <Route path="/editor" element={<div>Not implemented.</div>}/>
+      <Route path="/search" element={<div>Not implemented.</div>}/>
+      <Route path="/login" element={<div>Not implemented.</div>}/>
+    </Route>,
+    <Route path="/*" element={<div>404 not found.</div>}/>
+  ]));
+
+  return (
+    <AuthContext.Provider value={user}>
+      <RouterProvider router={router}/>
+    </AuthContext.Provider>
+  );
+}
