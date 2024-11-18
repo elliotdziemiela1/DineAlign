07e1a2fb4620974590175638b21081a21bea6ed5
Daniel Li <daniel39@illinois.edu>
2024-11-18 17:48:55 -0600

    Merged api branch into frontend and solved integration issue

---
+++ startapp.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -e
+
+echo "Setting up server dependencies..."
+cd ./server
+npm install
+rm -rf ./clientbuild || true
+
+echo "Building frontend..."
+cd ../client
+npm install
+npm run build
+mv ./build ../server/clientbuild
+
+echo "Starting server..."
+cd ../server
+npm start
