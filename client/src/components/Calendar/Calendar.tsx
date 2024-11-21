e19539afafa2b6320744248ab0c5e7c579ada024
Daniel Li <daniel39@illinois.edu>
2024-11-21 13:55:12 -0600

    Barebones feed

--- client/src/components/Calendar/Calendar.tsx
+++ client/src/components/Calendar/Calendar.tsx
@@ -16,6 +16,12 @@ export interface CalendarDay {
     mealEntries: Meal[];
 }
 
+export interface DayWithIndex {
+    index: number;
+    descriptor: string;
+    mealEntries: Meal[];
+}
+
 export enum Privacy {
     PRIVATE = 0,
     UNLISTED = 1,
