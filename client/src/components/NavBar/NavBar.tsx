0db34d2b1dd382dc763ca40730275eb5f4419654
elliotdziemiela1 <elliotanthony9@gmail.com>
2024-12-04 17:53:33 -0500

    Navbar styled

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -15,8 +15,8 @@ function NavBar() {
             <div className={styles.navbar}>
                 <p className={styles.logo} onClick={() => navigate("/home")}>Dine Align</p>
                 <input type="text" placeholder="Search.." className={styles.search} value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)}/>
-                <Link to={searchQuery ? `/searchUsers/${searchQuery}` : "#"} >Search Users</Link>
-                <Link to={searchQuery ? `/searchDiets/${searchQuery}` : "#"}>Search Diets</Link>
+                <Link to={searchQuery ? `/searchUsers/${searchQuery}` : "#"} className={styles.navLink}>Search Users</Link>
+                <Link to={searchQuery ? `/searchDiets/${searchQuery}` : "#"} className={styles.navLink}>Search Diets</Link>
                 <Link to={"/profile"} className={styles.profile}>Profile</Link>
                 {!!user.user ? <button type="button" onClick={() => signOut()} className={styles.login}>Sign Out</button> : 
                                 <button type="button" onClick={() => navigate("/login")} className={styles.login}>Sign In</button>}
