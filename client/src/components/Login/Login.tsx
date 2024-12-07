a6aa7418f1df0a8afef158d80a6aeb12af383653
baez5 <baez5@illinois.edu>
2024-12-06 21:07:36 -0600

    Styled login page + small changes

--- client/src/components/Login/Login.tsx
+++ client/src/components/Login/Login.tsx
@@ -3,6 +3,7 @@ import { signIn, signUp } from "../../services/auth";
 import { Navigate } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../..";
+import style from './Login.module.scss'
 
 export default function Login() {
     async function login(username:string, email: string, password: string, isLogin: boolean) {
@@ -30,23 +31,39 @@ export default function Login() {
         );
     }
     return (
-        <>
-            <p>Signup requires email, password, and unique username. Login only requires email and password.</p>
-            <input type="email" name="email" value={email} placeholder="email" onChange={(e) => setEmail(e.target.value)}/>
-            <input type="password" name="password" value={password} placeholder="password" onChange={(e) => setPassword(e.target.value)}/>
-            <input type="username" name="username" value={username} placeholder="username" onChange={(e) => setUsername(e.target.value)}/>
-            <button type="button" onClick={(evt) => {
-                evt.preventDefault();
-                setIsLoading(true);
-                login(username, email, password, true);
-            }}>Login</button>
-            <button type="button" onClick={(evt) => {
-                evt.preventDefault();
-                setIsLoading(true);
-                login(username, email, password, false);
-            }}>Sign Up</button>
-            {error.code !== "" && <p>{`${error.code}: ${error.message}`}</p>}
+        <div>
+            <div className={style.container}>
+                <div className={style.page}>
+                    <h1 className={style.pseudologo}>Dine Align</h1>
+                    <p>Signup requires email, password, and unique username. <br></br> Login only requires email and password.</p>
+                    <div className={style.inputBox}>
+                        <label className={style.labelIndent}>Email:</label>
+                        <input type="email" name="email" value={email} placeholder="example@example.com" onChange={(e) => setEmail(e.target.value)}/>
+                    </div>
+                    <div className={style.inputBox}>
+                        <label className={style.labelIndent}>Password:</label>
+                        <input type="password" name="password" value={password} placeholder="********" onChange={(e) => setPassword(e.target.value)}/>
+                    </div>
+                    <div className={style.inputBox}>
+                        <label className={style.labelIndent}>Username:</label>
+                        <input type="username" name="username" value={username} placeholder="name" onChange={(e) => setUsername(e.target.value)}/>
+                    </div>   
+                    <div className={style.buttonContainer}>
+                        <button className={style.signInButton} type="button" onClick={(evt) => {
+                            evt.preventDefault();
+                            setIsLoading(true);
+                            login(username, email, password, true);
+                        }}>Login</button>
+                        <button className={style.signInButton} type="button" onClick={(evt) => {
+                            evt.preventDefault();
+                            setIsLoading(true);
+                            login(username, email, password, false);
+                        }}>Sign Up</button>
+                    </div>
+                    {error.code !== "" && <p>{`${error.code}: ${error.message}`}</p>}
+                </div>
+            </div>
             
-        </>
+        </div>
     )
 }
