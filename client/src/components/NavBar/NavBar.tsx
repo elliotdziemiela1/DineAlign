84ce291401e1429e8b74b05c5a2c7343fe19cee3
Your Name <elliotanthony9@gmail.com>
2025-09-24 11:08:27 -0500

    Changed Logo Back to Dine Align

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -12,7 +12,7 @@ function NavBar() {
     return (
         <>
             <div className={styles.navbar}>
-                <p className={styles.logo} onClick={() => navigate("/home")}>nginx.conf not loading?</p>
+                <p className={styles.logo} onClick={() => navigate("/home")}>Dine Align</p>
                 <div className={styles.sbar}>
                     <input type="text" placeholder="Search.." className={styles.search} value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)}/>
                     <div className={styles.searchbutton}>
